7ee3bb0087d5107ccdc2fb989ab39e676b4f094f
Aamina Binte Khurram <114048193+aaminaa01@users.noreply.github.com>
2023-12-03 15:45:28 +0500

    Update search.php

--- search.php
+++ search.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Display Results</title>
-    <!-- Add your own styles or use Bootstrap for styling -->
-    <!-- Example Bootstrap CDN link -->
+    <!-- Bootstrap CDN link -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body class="bg-light">
