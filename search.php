76527bed69c9f984d81a29d1d2597f90ca611769
Aamina Binte Khurram <114048193+aaminaa01@users.noreply.github.com>
2023-12-04 00:10:07 +0500

    Include context in search results

--- search.php
+++ search.php
@@ -4,17 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Display Results</title>
-    <!-- Bootstrap CDN link -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body class="bg-light">
     <div class="container mt-5">
         <?php
-        include 'index.php';
         // Check if the search query is provided in the URL
             if (isset($_GET['query'])) {
                 $search_query = $_GET['query'];
-
 
                 echo "<h2 class='mb-4'>Search Results for '$search_query':</h2>"; // Bootstrap margin class
 
@@ -22,8 +19,10 @@
 
                 if (!empty($matching_urls)) {
                     echo "<ul class='list-group'>"; // Bootstrap list group class
-                    foreach ($matching_urls as $url) {
-                        echo "<li class='list-group-item'><a href='$url'>$url</a></li>"; // Bootstrap list group item class
+                    foreach ($matching_urls as $urlTitle => $context) {
+                        list($url, $title) = explode('|', $urlTitle); // Split the URL and title
+                        echo "<li class='list-group-item'><a href='" . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . "'>" . htmlspecialchars($title, ENT_QUOTES, 'UTF-8') . "</a><br><small>" . htmlspecialchars($context, ENT_QUOTES, 'UTF-8') . "</small></li>";
+
                     }
                     echo "</ul>";
                 } else {
@@ -35,13 +34,13 @@
             }
 
             function searchInDatabase($search_query) {
-                
+                $context_length = 100;
                 $matching_urls = [];
                 include 'db.php';
                 // Create a connection to the database
                 $conn = new mysqli($servername, $username, $password, $dbname);
 
-                $sql = "SELECT url, html_content FROM crawled_urls";
+                $sql = "SELECT url, title, html_content FROM crawled_urls";
 
                 $result = $conn->query($sql);
 
@@ -49,14 +48,26 @@
                     while ($row = $result->fetch_assoc()) {
                         $url = $row['url'];
                         $data = $row['html_content'];
+                        $title = $row['title'];
 
                         if (!empty($data) && stripos($data, $search_query) !== false) {
-                            $matching_urls[] = $url;
+                   
+                            // Find the position of the search query in the content
+                            $position = stripos($data, $search_query);
+            
+                            // Extract the context around the matched content
+                            $start_position = max(0, $position - $context_length);
+                            $end_position = min(strlen($data), $position + strlen($search_query) + $context_length);
+            
+                            $context = substr($data, $start_position, $end_position - $start_position);
+            
+                            // Add the URL and its context to the result array
+                            $matching_urls["$url|$title"] = strip_tags($context);
                         }
                     }
+                    
+                    return $matching_urls;
                 }
-
-                return $matching_urls;
             }
         ?>
     </div>
